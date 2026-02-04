8211874da8efe7b3dc1c455765daaa7b25f83756
FakeThird <thirdwheelerforhire@gmail.com>
2026-02-05 01:04:55 +0800

    feat: added test shell loop using fork, exit control, and input scanning/running

--- mysh.c
+++ mysh.c
@@ -3,8 +3,11 @@
  * A simple C program to verify your UNIX development environment.
  */
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 typedef struct {
     char *command;        // Command name
@@ -14,6 +17,8 @@ typedef struct {
     bool append;          // true for >>, false for >
     bool background;      // true if & present
 } Command;
+
+int max_args = 254;
 
 void print_command(Command *cmd) {
     printf("Command: %s\n", cmd->command);
@@ -41,18 +46,49 @@ int parse_command(char *input_line, Command *cmd) {
 int main() {
     // Example command: code "pattern" < input.txt > output.txt &
     Command cmd;
-    cmd.command = "echo";
-    cmd.args[0] = "echo";
-    cmd.args[1] = "pattern";
-    cmd.args[2] = NULL; // NULL-terminate the args array
-    cmd.input_file = "input.txt";
-    cmd.output_file = "output.txt";
-    cmd.append = false; // overwrite
-    cmd.background = true; // run in background
+    // cmd.command = "echo";
+    // cmd.args[0] = "print";
+    // cmd.args[1] = "pattern";
+    // cmd.args[2] = NULL; // NULL-terminate the args array
+    // cmd.input_file = "input.txt";
+    // cmd.output_file = "output.txt";
+    // cmd.append = false; // overwrite
+    // cmd.background = true; // run in background
+    // print_command(&cmd);
 
-    print_command(&cmd);
+    // currently assumes that we are using standard unix language
+    char arguments[100];
 
-    execpv(cmd.command, cmd.args);
+    printf("mysh> ");
+    fgets(arguments, sizeof(arguments), stdin);
+    arguments[strcspn(arguments, "\n")] = 0;
+
+    char *token;
+    token = strtok(arguments, " ");
+        
+    if (strcmp(token, "exit") == 0) {
+        return 0;
+    }
+
+    int pid = fork();
+
+    if (pid == 0) {
+        int counter = 0;
+        cmd.command = token;
+        while (token != NULL) {
+            cmd.args[counter] = token;
+            counter += 1;
+            token = strtok(NULL, " ");
+        }
+        execvp(cmd.command, cmd.args);
+
+        perror("execvp failed");
+        exit(EXIT_FAILURE);
+    } else {
+        // Parent process
+        waitpid(pid, NULL, 0);
+        main();
+    }
     return 0;
 }
 
