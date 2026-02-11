8f087736ad6d24e317c4858732cb74c2901beed4
“CL4-Bisk” <apariciojohnclyde@gmail.com>
2026-02-11 14:17:36 +0800

    feat: added I/O Redirection and Background Execution in both parser and interpreter

--- interpreter.c
+++ interpreter.c
@@ -12,6 +12,15 @@
 
 #include "header.h"
 
+/* maintains a structure of background jobs */
+typedef struct {
+    char background_jobs[256][256];     // Array to store background job commands
+    int job_counter;                    // Counter to keep track of the number of background jobs
+    pid_t job_pids[256];                // Array to store PIDs of background jobs
+} Background;
+
+Background bg = {.job_counter = 0};
+
 void print_command(Command *cmd) {
     printf("Command: %s\n", cmd->command);
     printf("Arguments:\n");
@@ -33,15 +42,18 @@ int interpret(Command *cmd){
     /* built-in commands */
     if (strcmp(cmd -> command, "exit") == 0) {
         return 1;
+
     } else if (strcmp(cmd -> command, "cd") == 0) {
         /* tokenizes argument after inputting for checking current directory */
         cmd -> command = strtok(NULL, " ");
-        if (cmd -> command == NULL) {
+        // for cd
+        if (cmd->args[1] == NULL) {
             fprintf(stderr, "cd: missing argument\n");
-        } else if (chdir(cmd -> command) != 0) {
+        } else if (chdir(cmd->args[1]) != 0) {
             perror("cd failed");
         }
         return 0;
+
     } else if (strcmp(cmd -> command, "pwd") == 0) {
         /* retrieves current working directory and prints */
         char cwd[MAX_ARGS];
@@ -52,16 +64,49 @@ int interpret(Command *cmd){
         }
         return 0;
     }
+
     int pid = fork();
 
     if (pid == 0) {
         execvp(cmd -> command, cmd -> args);
         perror("execvp failed");
         exit(EXIT_FAILURE);
+
     } else {
         // Parent process
-        waitpid(pid, NULL, 0);
-        printf("Child successful runned first.\n");
+        if (!cmd -> background) {
+            waitpid(pid, NULL, 0);
+        } else {
+
+            waitpid(pid, NULL, WNOHANG);
+            bg.job_counter++;
+            bg.background_jobs[bg.job_counter] = cmd -> command;
+            strcat(bg.background_jobs[bg.job_counter], cmd -> args[1]);
+            bg.job_pids[bg.job_counter] = pid;
+
+            printf("[%d] Started background job: %s (PID: %d)\n", bg.job_counter, bg.background_jobs[bg.job_counter], bg.job_pids[bg.job_counter]);
+
+            
+
+            // bg.job_counter++;
+            // bg.background_jobs[bg.job_counter] = cmd -> command;
+            // strcat(bg.background_jobs[bg.job_counter], cmd -> args[1]);
+            // bg.job_pids[bg.job_counter] = pid;
+            // printf("[%d]\t%d\n", bg.job_counter, pid);
+
+            // // Check for completed background jobs
+            // for (int i = bg.job_counter; i > 0; i--) {
+            //     int status;
+            //     pid_t result = waitpid(bg.job_pids[i], &status, WNOHANG);
+            //     if (result > 0) {
+            //         printf("[%d] Done \t%s\n", i, bg.background_jobs[i]);
+            //         // bg.background_jobs[i] = NULL; dunno abt this pero hambal ni sir na remove completed job from the list (ʘ ͟ʖ ʘ)
+            //     }
+            // }
+            
+        } 
+
+        // printf("Child successful runned first.\n");
     }
     return 0;
 }
--- parser.c
+++ parser.c
@@ -14,15 +14,50 @@
 Command parse(char *input){
     Command cmd;
 
+    cmd.command      = NULL;
+    cmd.input_file   = NULL;
+    cmd.output_file  = NULL;
+    cmd.append       = false;
+    cmd.background   = false;
+
+    for (int i = 0; i < MAX_ARGS; i++) {
+        cmd.args[i] = NULL;
+    }
+
     char *tokens = strtok(input, " ");
     /* adds the arguments into the command */
     int counter = 0;
-    cmd.command = tokens;
 
     while (tokens != NULL) {
-        cmd.args[counter++] = tokens;
+
+        if (strcmp(tokens, "<") == 0) {
+            tokens = strtok(NULL, " ");
+            cmd.input_file = tokens;
+
+        } else if (strcmp(tokens, ">") == 0) {
+            tokens = strtok(NULL, " ");
+            cmd.output_file = tokens;
+            cmd.append = false;
+
+        } else if (strcmp(tokens, ">>") == 0) {
+            tokens = strtok(NULL, " ");
+            cmd.output_file = tokens;
+            cmd.append = true;
+
+        } else if (strcmp(tokens, "&") == 0) {
+            cmd.background = true;
+
+        } else {
+            if (cmd.command == NULL) {
+                cmd.command = tokens;
+            }
+            cmd.args[counter++] = tokens;
+            
+        }
+
         tokens = strtok(NULL, " ");
     }
+    
     cmd.args[counter] = NULL;
     return cmd;
 }
